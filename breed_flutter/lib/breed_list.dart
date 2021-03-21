e28915eebeb4378737cefed8429fbae9e917a672
Tom Koptel <tom.koptel@gmail.com>
2021-03-21 21:39:39 +0100

    Generate list of breeds

--- breed_flutter/lib/breed_list.dart
+++ breed_flutter/lib/breed_list.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'breed.dart';
 
 class BreedList extends StatefulWidget {
-  BreedList({Key? key}) : super(key: key);
+  BreedList({Key? key, required this.entries}) : super(key: key);
+  final List<Breed> entries;
 
   @override
   _BreedListState createState() => _BreedListState();
@@ -11,27 +12,50 @@ class BreedList extends StatefulWidget {
 class _BreedListState extends State<BreedList> {
   @override
   Widget build(BuildContext context) {
-    final List<String> entries = <String>['Dog 1', 'Dog 2', 'Dog 3'];
-
     return ListView.builder(
         padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
+        itemCount: widget.entries.length,
         itemBuilder: (BuildContext context, int index) {
           return BreedItem(
-              item: Breed(name: '${entries[index]}', isFavorite: false));
+            item: widget.entries[index],
+            onFavorited: () {
+              setState(() {
+                final entry = widget.entries[index];
+                final newEntry = entry.copyWith(isFavorite: !entry.isFavorite);
+                widget.entries[index] = newEntry;
+              });
+            },
+          );
         });
   }
 }
 
 class BreedItem extends StatelessWidget {
-  const BreedItem({Key? key, required this.item}) : super(key: key);
+  const BreedItem({Key? key, required this.item, this.onFavorited})
+      : super(key: key);
   final Breed item;
+  final VoidCallback? onFavorited;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      child: Text(item.name ?? ""),
+    final IconData iconVal =
+        item.isFavorite ? Icons.favorite : Icons.favorite_outline;
+
+    var container = Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(item.name ?? ""),
+          IconButton(
+            icon: Icon(iconVal),
+            tooltip: 'Add breed to favorites',
+            onPressed: onFavorited,
+          ),
+        ],
+      ),
     );
+    return container;
   }
 }
--- breed_flutter/lib/home.dart
+++ breed_flutter/lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'breed.dart';
 import 'breed_list.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,12 +10,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final uuid = Uuid();
+    final entries = <Breed>[
+      Breed(uuid.v4(), name: 'Dog 1', isFavorite: true),
+      Breed(uuid.v4(), name: 'Dog 2', isFavorite: false),
+      Breed(uuid.v4(), name: 'Dog 3', isFavorite: false),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title!),
       ),
       body: Center(
-        child: BreedList(),
+        child: BreedList(entries: entries),
       ),
     );
   }
