bac866c2b43c45427468aee1dbaa0299f79d1718
tomkoptel <tom.koptel@gmail.com>
2021-04-23 21:27:50 +0200

    Update iOS apps Xcode projects (#25)

--- BreedKN/ios/Pods/Target Support Files/Pods-BreedKN/Pods-BreedKN-umbrella.h
+++ BreedKN/ios/Pods/Target Support Files/Pods-BreedKN/Pods-BreedKN-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_bai_dialog_iosVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_bai_dialog_iosVersionString[];
+FOUNDATION_EXPORT double Pods_BreedKNVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BreedKNVersionString[];
 
