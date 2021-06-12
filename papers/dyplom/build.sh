d4e3627dbcf08cc5c1653134be36246568b4ff2e
Tom Koptel <tom.koptel@gmail.com>
2021-06-12 16:10:09 +0200

    Try to fix find -E

--- papers/dyplom/build.sh
+++ papers/dyplom/build.sh
@@ -1,3 +1,31 @@
-#!/bin/bash
-./build_paper.sh
-./clear.sh
+
+# Pdflatex
+PDFLATEX='pdflatex -interaction=nonstopmode -shell-escape -file-line-error'
+GREP='grep ".*:[0-9]*:.*"' # показывает на выходе только ошибки
+
+# Файлы/Папки
+PDF_NAME='dyplom.pdf'
+TEX='tex'
+IMG='tex/img'
+MAINTEX='main'
+
+# Конвертация eps
+find -regextype posix-extended $IMG/ -type f -name "*.eps" -exec epstopdf {} ";" ;
+find -regextype posix-extended $IMG -type f -name "*.eps" -exec rm -f {} \;
+
+# Сборка latex
+cd tex
+$PDFLATEX $MAINTEX > /dev/null
+BIBOUTPUT=$(bibtex $MAINTEX)
+# Показывать output bibtex'a только в случае ошибок
+if [[ "$BIBOUTPUT" =~ "error" ]]; then
+    echo "$BIBOUTPUT"
+fi
+$PDFLATEX $MAINTEX > /dev/null
+$PDFLATEX $MAINTEX
+
+cp $MAINTEX.pdf ../$PDF_NAME
+cd ..
+
+# Clear
+find -regextype posix-extended $TEX/ -maxdepth 1 -type f ! -regex ".*\.(tex|log|blg|bib|cls|sty|bst|clo|asm|gitignore)" -exec rm -f {} \; ;
--- papers/dyplom/clear.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Clear
-find -E $TEX/ -maxdepth 1 -type f ! -regex ".*\.(tex|log|blg|bib|cls|sty|bst|clo|asm|gitignore)" -exec rm -f {} \; ;
